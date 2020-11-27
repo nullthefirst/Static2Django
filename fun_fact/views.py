3d4a3a8f598fb186ad9d5e113c0570df0ed2fc96
Ninte <ninte.dev@gmail.com>
2020-11-27 15:40:36 +0100

    added result focus view

--- fun_fact/views.py
+++ fun_fact/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 
 from .models import FunFactSubmission
 from .forms import SubmissionForm
@@ -18,3 +18,13 @@ class Results(ListView):
     model = FunFactSubmission
     ordering = ['-id']
     context_object_name = 'submissions'
+    
+
+class ResultFocus(DetailView):
+    template_name = 'pages/result-focus.html'
+    model = FunFactSubmission
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['result'] = FunFactSubmission.objects.get(pk=self.kwargs['pk'])
+        return context
