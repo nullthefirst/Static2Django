48da02ac482bab724c0d2369b6f8e857ce8cde05
Ninte <ninte.dev@gmail.com>
2020-12-04 17:45:05 +0100

    added whitenoise config

--- static_django/settings.py
+++ static_django/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,3 +147,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # static files directory
+
+# Compressed, but not cached.
+# This is preferred for ease of usage in simple deployments.
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
